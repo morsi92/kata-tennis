56beb78be338ef788c997853106b75280d80a5a4
morsi abdelghani <abdelghani.morsi@outlook.fr>
2020-09-13 17:44:45 +0200

    add failing test to display set score when set starts

---
+++ src/main/java/model/Set.java
@@ -0,0 +1,15 @@
+package model;
+
+public class Set {
+    private Player playerOne;
+    private Player playerTwo;
+
+    public Set(Player playerOne, Player playerTwo) {
+        this.playerOne = playerOne;
+        this.playerTwo = playerTwo;
+    }
+
+    public String getScore() {
+        return null;
+    }
+}
---
+++ src/test/java/model/SetTest.java
@@ -0,0 +1,24 @@
+package model;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class SetTest {
+
+    @Test
+    public void set_scores_should_be_zero_when_start_new_set() {
+        // Given
+        String playerOneName = "player one name";
+        String playerTwoName = "player two name";
+        Player playerOne = new Player(playerOneName);
+        Player playerTwo = new Player(playerTwoName);
+        String expectedSetScore = playerOneName + " " + "0" + "\n" + playerTwoName + " " + "0";
+
+        // When
+        Set set = new Set(playerOne, playerTwo);
+
+        // Then
+        Assertions.assertThat(set.getScore()).isEqualTo(expectedSetScore);
+    }
+
+}
